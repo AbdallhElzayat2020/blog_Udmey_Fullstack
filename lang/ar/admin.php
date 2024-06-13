615aac0722e05b35042ce2f9d9011a9cf6b18f35
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-06-13 03:33:54 +0300

    Localization Multi Language Admin

--- lang/ar/admin.php
+++ lang/ar/admin.php
@@ -1,1 +1,13 @@
 <?php
+
+    return [
+        'Login' => 'تسجيل الدخول' ,
+        'Email' => 'البريد الالكتروني' ,
+        'Please fill in your email' => 'يرجى ملء البريد الإلكتروني الخاص بك' ,
+        'Password' => 'كلمة المرور' ,
+        'Forgot Password?' => 'هل نسيت كلمة السر؟' ,
+        'please fill in your password' => 'يرجى ملء كلمة المرور الخاصة بك' ,
+        'Remember Me' => 'تذكرنى' ,
+
+    ];
+?>
---
+++ lang/en/admin.php
@@ -0,0 +1,13 @@
+<?php
+
+    return [
+        'Login' => 'Login' ,
+        'Email' => 'Email' ,
+        'Please fill in your email' => 'Please fill in your email' ,
+        'Password' => 'Password' ,
+        'Forgot Password?' => 'Forgot Password?' ,
+        'please fill in your password' => 'please fill in your password' ,
+        'Remember Me' => 'Remember Me' ,
+
+    ];
+?>
