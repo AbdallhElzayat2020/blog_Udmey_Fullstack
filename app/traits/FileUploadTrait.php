30d1cf8aa441fb3073855fa0a8b7a937ec685156
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-06-14 00:42:36 +0300

    Trait handleFileUpload

--- app/traits/FileUploadTrait.php
+++ app/traits/FileUploadTrait.php
@@ -4,19 +4,59 @@
 
     use Illuminate\Http\File;
     use Illuminate\Http\Request;
+    use Illuminate\Support\Str;
 
     trait FileUploadTrait
     {
-        public function handleFileUpload( Request $request , $dir = 'uploads' , $oldPath = null )
-        {
-            if ($oldPath && File::exists(public_path($oldPath))) {
-                File::delete(public_path($oldPath));
-            }
-//            if ($request->hasFile('file')) {
-//                $file = $request->file('file');
-//                $fileName = $file->getClientOriginalName();
-//                $file->move(public_path($dir), $fileName);
-//                return $dir . '/' . $fileName;
+//        public function handleFileUpload( Request $request , $fileName , $directory = 'uploads' , $oldPath = null )
+//        {
+//            //  delete if img exists
+//            if ($oldPath && File::exists(public_path($oldPath))) {
+//
+//                File::delete(public_path($oldPath));
 //            }
+////            check if Have file
+//            if ( !$request->hasFile($fileName)) {
+//
+//                return null;
+//            }
+//
+//            $file = $request->file($fileName);
+//
+//            $extension = $file->getClientOriginalExtension();
+//
+//            $updatedFileName = Str::random(20) . '.' . $extension;
+//
+//            $file->move(public_path($directory) , $updatedFileName);
+//            $filePath = $directory . '/' . $updatedFileName;
+//
+//            return $filePath;
+//
+//        }
+
+
+        public function handleFileUpload( Request $request , $fileName , $dir = 'uploads' , $oldPath = null )
+        {
+//            check if file exists
+            if ($oldPath && File::extists(public_path($oldPath))) {
+
+                File::delte(public_path($oldPath));
+            }
+//            check if request has img
+            if ( !$request->hasFile($fileName)) {
+                return null;
+            }
+
+            $file = $request->file($fileName);
+
+            $extension = $file->getClientOriginalExtension();
+
+            $updatedFileName = Str::random(20) . '.' . $extension;
+
+            $file->move(public_path($dir) , $updatedFileName);
+
+            $filePath = $dir . '/' . $updatedFileName;
+
+            return $filePath;
         }
     }
